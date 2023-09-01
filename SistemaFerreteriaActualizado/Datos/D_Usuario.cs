bcd70332e4ab5f7b41d741a84def3879a7710d9a
Matiaja <102177224+Matiaja@users.noreply.github.com>
2023-09-01 19:56:54 -0300

    Add files via upload

---
+++ SistemaFerreteriaActualizado/Datos/D_Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Datos
+{
+
+    public class MiDbContext : DbContext
+    {
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Empleado> Empleadoes { get; set; }
+    }
+
+    public class D_Usuario
+    {
+        public Usuario ObtenerUsuarioPorDNI(string dni)
+        {
+            using (var context = new MiDbContext())
+            {
+                return context.Usuarios.FirstOrDefault(u => u.Dni == dni);
+            }
+        }
+
+
+    }
+}
