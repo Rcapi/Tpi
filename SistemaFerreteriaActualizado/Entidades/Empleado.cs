e4c81de7842e1714fc4c901995672c8df0137435
Matiaja <102177224+Matiaja@users.noreply.github.com>
2023-09-01 19:58:36 -0300

    Add files via upload

---
+++ SistemaFerreteriaActualizado/Entidades/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class Empleado : Usuario
+    {
+        [Key]
+        public int Legajo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ SistemaFerreteriaActualizado/Negocio/GestorUsuario.cs
@@ -0,0 +1,30 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Negocio
+{
+    /*
+    public class GestorUsuario
+    {
+        private D_Usuario d_Usuario = new D_Usuario();
+
+        public bool ValidarUsuario(string Dni, string claveIngresada)
+        {
+            Datos.Usuario usuario = d_Usuario.ObtenerUsuarioPorDNI(Dni);
+
+            if (usuario != null)
+            {
+                return usuario.Clave == claveIngresada;
+            }
+
+            return false;
+        }
+    }
+    */
+
+}
